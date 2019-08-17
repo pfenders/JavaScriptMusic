267eac451427ecf0a79044a7f8da27d34406a83c
pfenders <pfenders3123@gmail.com>
2019-08-17 02:03:46 -0700

    update

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector(".visual");
-    const colors = [ '#eef113','#791a0d','#a032e0', '#f71fda', '#0eb358', '#0e7fb3' ]
+    const colors = ['#eef113', '#791a0d', '#a032e0', '#f71fda', '#0eb358', '#0e7fb3'];
 
-    //the sounds
+    //Make Sounds
     pads.forEach((pad, index) => {
         pad.addEventListener('click', function () {
             sounds[index].currentTime = 0;
             sounds[index].play();
 
+            createBubbles(index);
         });
     });
+
+    //Make Bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener('animationend', function () {
+            visual.removeChild(this);
+        });
+    };
 });
